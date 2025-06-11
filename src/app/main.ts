6f009c0fdd609ca22dba6532f18c23aae715f415
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2025-06-11 18:57:32 -0500

    changing the static values for dynamic values by using the faker library

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,13 +1,30 @@
-import { addProduct } from "./products/product.service";
+import { faker }  from '@faker-js/faker';
 
-addProduct({
-  id: "1",
-  title: "Product 1",
-  createdAt: new Date(),
-  stock: 90,
-  size: "M",
-  category: {
-    id: "A",
-    name: "Category 1"
-  }
-})
+import { addProduct, products } from "./products/product.service";
+
+for (let i = 0; i < 50; i++) {
+  addProduct({
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    color: faker.color.human(),
+    size: faker.helpers.arrayElement(["S", "M", "L", "XL"]),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.lorem.words(faker.number.int({ min: 0, max: 3})).split(' '),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 10, max: 100 }),
+    category: {
+      id: faker.string.uuid(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+      name: faker.commerce.department()
+    }
+  });
+}
+
+console.log(products);
+// This code generates 50 fake product entries using the Faker.js library and adds them to the products array.
+
