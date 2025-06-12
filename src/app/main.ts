50252a1d65e3e7fdab57290b07386472f4121ac8
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2025-06-11 19:23:47 -0500

    adding the omit label to exclude some inputs from the addProduct function

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,7 +4,6 @@ import { addProduct, products } from "./products/product.service";
 
 for (let i = 0; i < 50; i++) {
   addProduct({
-    id: faker.string.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.url(),
     color: faker.color.human(),
@@ -13,15 +12,8 @@ for (let i = 0; i < 50; i++) {
     isNew: faker.datatype.boolean(),
     tags: faker.lorem.words(faker.number.int({ min: 0, max: 3})).split(' '),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.number.int({ min: 10, max: 100 }),
-    category: {
-      id: faker.string.uuid(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-      name: faker.commerce.department()
-    }
+    categoryId: faker.string.uuid(),
   });
 }
 
